6f30421b5d1777a402181dc31dd4ac93130ff25f
Qwaterberk <crazyvoid405@gmail.com>
2026-01-17 20:47:22 +0300

    Add FirstScanner
    this is my First Scanner

---
+++ FirstScanner.java
@@ -0,0 +1,14 @@
+// ֲ Ýׂ־ּ Ê־ִֵ ֲ־־ֱÙֵ ָֽ׳ֳֵ־ ֵֽ ׂ׀־ְֳֹ!!!
+import java.util.Scanner;
+
+public class FirstScanner {
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("ֲגוהט סגמ¸ טלÿ:");
+  String name = sc.nextLine();
+  System.out.println("ֲגוהט סגמי גמחנאסע:");
+  int age = sc.nextInt();
+  System.out.println("Name: "+name);
+  System.out.println("Age: "+age);
+ }
+}
